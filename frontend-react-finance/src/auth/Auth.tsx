77e9e76c82b95e1a1743ef6916ce371460a32956
Gaura Palande <Gaurav.Palande147@gmail.com>
2025-06-13 14:01:06 -0700

    hard vercel redirect fix

--- frontend-react-finance/src/auth/Auth.tsx
+++ frontend-react-finance/src/auth/Auth.tsx
@@ -4,6 +4,10 @@ const Auth = () => {
   const signInWithGoogle = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
+      options: {
+    redirectTo: 'https://stingy-hubby-frontend-react-finance.vercel.app'
+'
+  }
     })
     if (error) console.error('OAuth login error:', error.message)
   }
