428d1c538362b6b86d8330a283721d436cc73404
Gaura Palande <Gaurav.Palande147@gmail.com>
2025-06-15 09:18:14 -0700

    Added hcaptcha to auth buttons

--- frontend-react-finance/src/components/AuthButtons.tsx
+++ frontend-react-finance/src/components/AuthButtons.tsx
@@ -1,15 +1,28 @@
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { supabase } from '../supabaseClient';
+import HCaptcha from '@hcaptcha/react-hcaptcha';
+import React, { useRef, useState } from 'react';
 
 const AuthButtons = () => {
+  const [captchaToken, setCaptchaToken] = useState<string | null>(null);
+  const captchaRef = useRef<HCaptcha>(null);
+
   return (
-    <Auth
-      supabaseClient={supabase}
-      appearance={{ theme: ThemeSupa }}
-      providers={['google']}
-      redirectTo={window.location.origin} // Ensure the redirect URL is set to your app's origin
-    />
+    <>
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={['google']}
+        redirectTo={window.location.origin} // Ensure the redirect URL is set to your app's origin
+      />
+
+      <HCaptcha
+        sitekey="2c02d91c-a64a-4124-85b0-d1cc928898e4" // replace with your actual sitekey
+        onVerify={(token: string) => setCaptchaToken(token)}
+        ref={captchaRef}
+      />
+    </>
   );
 };
 
