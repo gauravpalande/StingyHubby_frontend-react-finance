7ab2e39f56700d801567f56c9fb1d56f3d8b2e62
Gaura Palande <Gaurav.Palande147@gmail.com>
2025-06-16 20:38:19 -0700

    Reduced width

--- frontend-react-finance/src/pages/HomePage.tsx
+++ frontend-react-finance/src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ import { ThemeSupa } from '@supabase/auth-ui-shared';
 import EmailAuthWithCaptcha from '../components/EmailAuthWithCaptcha';
 import SidebarLayout from '../components/SidebarLayout'; // Import SidebarLayout
 
-const sidebarWidth = 150;
+const sidebarWidth = 100;
 
 const HomePage: React.FC = () => {
   const supabase = useSupabaseClient();
